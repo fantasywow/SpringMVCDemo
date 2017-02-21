22e570e2f543e82382ec4a6c608520beace78910
liu <liutianhe519@gmail.com>
2017-02-21 16:05:02 +0800

    add spring session.   done

---
+++ src/main/java/liu/spring/SessionInitializer.java
@@ -0,0 +1,9 @@
+package liu.spring;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+/**
+ * Created by wtswindows7 on 2017/2/21.
+ */
+public class SessionInitializer extends AbstractHttpSessionApplicationInitializer {
+}
