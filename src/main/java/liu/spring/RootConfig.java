52bcff165c2cbd8614deebf7381c5d0a646dc926
liu <liutianhe519@gmail.com>
2017-05-22 21:43:25 +0800

    add maven-war-plugin

--- src/main/java/liu/spring/RootConfig.java
+++ src/main/java/liu/spring/RootConfig.java
@@ -68,25 +68,25 @@ public class RootConfig {
 
     }
 
-    @Bean
-    public ProducerFactory<Integer, String> producerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(props);
-    }
-
-
-    @Bean
-    public KafkaTemplate<Integer, String> kafkaTemplate() {
-        return new KafkaTemplate<Integer, String>(producerFactory());
-    }
+//    @Bean
+//    public ProducerFactory<Integer, String> producerFactory() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+//        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+//        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+//        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//
+//        return new DefaultKafkaProducerFactory<>(props);
+//    }
+//
+//
+//    @Bean
+//    public KafkaTemplate<Integer, String> kafkaTemplate() {
+//        return new KafkaTemplate<Integer, String>(producerFactory());
+//    }
 
 
 
